1e7f14b6875e169dfc8e19c43a8a3146b08aa58d
Hieu Rocker <rockerhieu@gmail.com>
2016-03-14 11:45:41 +0700

    Implement infinite scroll and error view

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -12,43 +12,111 @@ import SwiftyJSON
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var errorView: UIView!
-
+    
+    var loadingMoreView:InfiniteScrollActivityView?
+    var refreshControl: UIRefreshControl!
+    
+    var isMoreDataLoading = false
+    var page = 0
     var movies: [JSON]?
     var endpoint: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "loadMovies:", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.insertSubview(refreshControl, atIndex: 0)
         tableView.delegate = self
         tableView.dataSource = self
+
+        setupErrorView()
+        setupRefreshControl()
+        setupInfiniteScrollLoadingIndicator()
         loadMovies(refreshControl)
     }
 
+    func setupRefreshControl() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "loadMovies:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+    }
+    
+    func setupInfiniteScrollLoadingIndicator() {
+        loadingMoreView = InfiniteScrollActivityView(frame: self.getLoadingMoreViewFrame())
+        loadingMoreView!.hidden = true
+        tableView.addSubview(loadingMoreView!)
+        
+        var insets = tableView.contentInset;
+        insets.bottom += InfiniteScrollActivityView.defaultHeight * 2;
+        tableView.contentInset = insets
+    }
+    
+    func setupErrorView() {
+        errorView.hidden = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: Selector("onErrorViewTap:"))
+        errorView.addGestureRecognizer(tapGesture)
+    }
+    
+    func onErrorViewTap(sender: AnyObject) {
+        loadMovies(refreshControl, pageIndex: page)
+    }
+
     func loadMovies(refreshControl: UIRefreshControl) {
-        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        Alamofire.request(.GET, Settings.apiRoot + endpoint!, parameters: ["api_key": Settings.apiKey])
+        loadMovies(refreshControl, pageIndex: 1)
+    }
+
+    func loadMovies(refreshControl: UIRefreshControl, pageIndex: Int) {
+        page = pageIndex
+        errorView.hidden = true
+        if (pageIndex == 1) {
+            MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        }
+        Alamofire.request(.GET, "\(Settings.apiRoot + endpoint!)", parameters: ["api_key": Settings.apiKey, "page": pageIndex])
             .validate().responseJSON { response in
-                MBProgressHUD.hideHUDForView(self.view, animated: true)
                 switch response.result {
                 case .Success:
                     if let value = response.result.value {
                         let json = JSON(value)
-                        self.movies = json["results"].array
+                        let movies = json["results"].array
+                        if (self.movies == nil || pageIndex == 1) {
+                            self.movies = movies
+                        } else {
+                            self.movies! += movies!
+                        }
                         self.tableView.reloadData()
                         self.errorView.hidden = true
+                        self.page = json["page"].int ?? 1
                     }
                     break
-                case .Failure(let error):
+                case .Failure(_):
                     self.errorView.hidden = false
                     break
                 }
+                if (pageIndex == 1) {
+                    MBProgressHUD.hideHUDForView(self.view, animated: true)
+                }
+                self.loadingMoreView!.stopAnimating()
                 refreshControl.endRefreshing()
+                self.isMoreDataLoading = false
         }
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.dragging) {
+                isMoreDataLoading = true
+                loadingMoreView?.frame = getLoadingMoreViewFrame()
+                loadingMoreView!.startAnimating()
+                loadMovies(refreshControl, pageIndex: page + 1)
+            }
+        }
+    }
+    
+    func getLoadingMoreViewFrame() -> CGRect {
+        return CGRectMake(0, tableView.contentSize.height, tableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,11 +130,10 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let overview = movie["overview"].string
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.posterView.image = nil
         if let posterPath = movie["poster_path"].string {
             let imageUrl = NSURL(string: Settings.endpointImage + posterPath)
             cell.posterView.setImageWithURL(imageUrl!)
-        } else {
-            cell.posterView.setImageWithURL(NSURL())
         }
         return cell
     }
@@ -78,5 +145,4 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let movieViewController = segue.destinationViewController as! MovieViewController
         movieViewController.movie = movie
     }
-
 }
